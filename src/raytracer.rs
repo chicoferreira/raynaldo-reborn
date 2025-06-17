e44109b5cdff2edc721898c32bdc172aff058351
chico ferreira <36338391+chicoferreira@users.noreply.github.com>
2025-06-17 20:52:08 +0100

    Remove recursion of render_ray

--- src/raytracer.rs
+++ src/raytracer.rs
@@ -40,35 +40,44 @@ impl Scene {
         let mut color = Vec4::ZERO;
         for _ in 0..samples_per_pixel {
             let ray = self.camera.generate_ray(x, y, &mut rng());
-            color += self.render_ray(&ray, max_depth);
+            color += self.render_ray(ray, max_depth);
         }
         color / samples_per_pixel as f32
     }
 
     pub fn render_sample(&self, x: u32, y: u32, max_depth: u32) -> Vec4 {
         let ray = self.camera.generate_ray(x, y, &mut rng());
-        self.render_ray(&ray, max_depth)
+        self.render_ray(ray, max_depth)
     }
 
-    fn render_ray(&self, ray: &Ray, max_depth: u32) -> Vec4 {
+    fn render_ray(&self, mut ray: Ray, max_depth: u32) -> Vec4 {
         if max_depth == 0 {
             return Vec4::ZERO;
         }
 
-        if let Some(result) = self.tracer.trace(ray, &(0.0001..)) {
-            let geometry = &self.world.geometry[result.geometry_index];
-            let material = &geometry.material;
+        let mut color = Vec4::ONE;
 
-            if let Some(scatter_result) = material.scatter(ray, &result) {
-                let scattered = scatter_result.scattered;
-                let attenuation = scatter_result.attenuation;
-                return attenuation * self.render_ray(&scattered, max_depth - 1);
+        for _ in 0..max_depth {
+            if let Some(result) = self.tracer.trace(&ray, &(0.0001..)) {
+                let geometry = &self.world.geometry[result.geometry_index];
+                let material = &geometry.material;
+
+                if let Some(scatter_result) = material.scatter(&ray, &result) {
+                    ray = scatter_result.scattered;
+                    color *= scatter_result.attenuation;
+                } else {
+                    return Vec4::ZERO;
+                }
+            } else {
+                return color * self.get_environment_color(&ray);
             }
-
-            return Vec4::ZERO;
         }
+
+        Vec4::ZERO
+    }
+
+    fn get_environment_color(&self, ray: &Ray) -> Vec4 {
         let t = 0.5 * (ray.direction.normalize().y + 1.0);
-        let color = vec4(1.0, 1.0, 1.0, 1.0) * (1.0 - t) + vec4(0.5, 0.7, 1.0, 1.0) * t;
-        color
+        vec4(1.0, 1.0, 1.0, 1.0) * (1.0 - t) + vec4(0.5, 0.7, 1.0, 1.0) * t
     }
 }
