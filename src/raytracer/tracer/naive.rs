cf8e6efdbf840679111a64dff7819bf8edf5fa2a
chico ferreira <36338391+chicoferreira@users.noreply.github.com>
2025-06-18 21:53:49 +0100

    Add triangle intersection test on naive tracer (closes #3)

--- src/raytracer/tracer/naive.rs
+++ src/raytracer/tracer/naive.rs
@@ -9,23 +9,51 @@ pub struct NaiveTracer {
 }
 
 impl NaiveTracer {
-    pub fn new(objects: &[GeometryType]) -> Self {
-        let objects = objects
-            .into_iter()
-            .flat_map(|o| match o {
-                GeometryType::Sphere { center, radius } => Some(NaiveObject::NaiveSphere {
-                    center: *center,
-                    radius: *radius,
-                }),
-                GeometryType::Quad {
-                    origin: point,
-                    u,
-                    v,
-                } => Some(NaiveObject::NaiveQuad(NaiveQuad::new(*point, *u, *v))),
-                // TODO: Implement triangle mesh intersection
-                GeometryType::TriangleMesh(_) => None,
-            })
-            .collect();
+    pub fn new(geometry: &[GeometryType]) -> Self {
+        let mut objects = Vec::new();
+
+        for (index, geometry) in geometry.iter().enumerate() {
+            match geometry {
+                GeometryType::Sphere { center, radius } => {
+                    objects.push(NaiveObject {
+                        geometry_index: index,
+                        geometry: NaiveGeometry::Sphere {
+                            center: *center,
+                            radius: *radius,
+                        },
+                    });
+                }
+                GeometryType::Quad { origin, u, v } => {
+                    let normal = u.cross(*v).normalize();
+                    let d = normal.dot(*origin);
+
+                    objects.push(NaiveObject {
+                        geometry_index: index,
+                        geometry: NaiveGeometry::Quad {
+                            origin: *origin,
+                            u: *u,
+                            v: *v,
+                            normal,
+                            d,
+                        },
+                    });
+                }
+                GeometryType::TriangleMesh(mesh) => {
+                    let mut objects = Vec::new();
+                    for (v1, v2, v3) in mesh.indices.iter() {
+                        let p1 = mesh.verts[*v1 as usize].into();
+                        let p2 = mesh.verts[*v2 as usize].into();
+                        let p3 = mesh.verts[*v3 as usize].into();
+
+                        objects.push(NaiveObject {
+                            geometry_index: index,
+                            geometry: NaiveGeometry::Triangle { p1, p2, p3 },
+                        });
+                    }
+                }
+            }
+        }
+
         Self { objects }
     }
 }
@@ -33,8 +61,8 @@ impl NaiveTracer {
 impl NaiveTracer {
     pub fn trace(&self, ray: &Ray, range: &impl RangeBounds<f32>) -> Option<TraceResult> {
         let mut closest_hit: Option<TraceResult> = None;
-        for (index, object) in self.objects.iter().enumerate() {
-            if let Some(hit) = object.hit(index, ray, range) {
+        for object in self.objects.iter() {
+            if let Some(hit) = object.hit(object.geometry_index, ray, range) {
                 if let Some(ref mut closest_hit) = closest_hit {
                     if hit.distance < closest_hit.distance {
                         *closest_hit = hit;
@@ -48,9 +76,28 @@ impl NaiveTracer {
     }
 }
 
-enum NaiveObject {
-    NaiveSphere { center: Vec3, radius: f32 },
-    NaiveQuad(NaiveQuad),
+struct NaiveObject {
+    geometry_index: usize,
+    geometry: NaiveGeometry,
+}
+
+enum NaiveGeometry {
+    Sphere {
+        center: Vec3,
+        radius: f32,
+    },
+    Quad {
+        origin: Vec3,
+        u: Vec3,
+        v: Vec3,
+        normal: Vec3,
+        d: f32,
+    },
+    Triangle {
+        p1: Vec3,
+        p2: Vec3,
+        p3: Vec3,
+    },
 }
 
 impl NaiveObject {
@@ -60,11 +107,20 @@ impl NaiveObject {
         ray: &Ray,
         range: &impl RangeBounds<f32>,
     ) -> Option<TraceResult> {
-        match self {
-            NaiveObject::NaiveSphere { center, radius } => {
+        match &self.geometry {
+            NaiveGeometry::Sphere { center, radius } => {
                 Self::intersect_sphere(*center, *radius, my_index, ray, range)
             }
-            NaiveObject::NaiveQuad(quad) => Self::intersect_quad(quad, my_index, ray, range),
+            NaiveGeometry::Quad {
+                origin,
+                u,
+                v,
+                normal,
+                d,
+            } => Self::intersect_quad(*origin, *u, *v, *normal, *d, my_index, ray, range),
+            NaiveGeometry::Triangle { p1, p2, p3 } => {
+                Self::intersect_triangle(*p1, *p2, *p3, my_index, ray, range)
+            }
         }
     }
 
@@ -119,39 +175,39 @@ impl NaiveObject {
     }
 
     fn intersect_quad(
-        quad: &NaiveQuad,
+        origin: Vec3,
+        u: Vec3,
+        v: Vec3,
+        normal: Vec3,
+        d: f32,
         geometry_index: usize,
         ray: &Ray,
         range: &impl RangeBounds<f32>,
     ) -> Option<TraceResult> {
-        let denominator = quad.normal.dot(ray.direction);
+        let denominator = normal.dot(ray.direction);
 
         if denominator.abs() < 1e-6 {
             return None;
         }
 
-        let distance = (quad.d - quad.normal.dot(ray.origin)) / denominator;
+        let distance = (d - normal.dot(ray.origin)) / denominator;
         if !range.contains(&distance) {
             return None;
         }
 
         let point = ray.at(distance);
-        let vector_in_plane = point - quad.origin;
+        let vector_in_plane = point - origin;
 
-        let u_coord = vector_in_plane.dot(quad.u) / quad.u.length_squared();
-        let v_coord = vector_in_plane.dot(quad.v) / quad.v.length_squared();
+        let u_coord = vector_in_plane.dot(u) / u.length_squared();
+        let v_coord = vector_in_plane.dot(v) / v.length_squared();
 
         if u_coord < 0.0 || u_coord > 1.0 || v_coord < 0.0 || v_coord > 1.0 {
             return None;
         }
 
-        let front_face = ray.direction.dot(quad.normal) < 0.0;
+        let front_face = ray.direction.dot(normal) < 0.0;
 
-        let hit_normal = if front_face {
-            quad.normal
-        } else {
-            -quad.normal
-        };
+        let hit_normal = if front_face { normal } else { -normal };
 
         Some(TraceResult {
             distance,
@@ -162,26 +218,53 @@ impl NaiveObject {
             uv: (u_coord, v_coord),
         })
     }
-}
 
-struct NaiveQuad {
-    origin: Vec3,
-    u: Vec3,
-    v: Vec3,
-    normal: Vec3,
-    d: f32,
-}
-
-impl NaiveQuad {
-    fn new(origin: Vec3, u: Vec3, v: Vec3) -> Self {
-        let normal = u.cross(v).normalize();
-        let d = normal.dot(origin);
-        Self {
-            origin,
-            u,
-            v,
-            normal,
-            d,
+    fn intersect_triangle(
+        p1: Vec3,
+        p2: Vec3,
+        p3: Vec3,
+        geometry_index: usize,
+        ray: &Ray,
+        range: &impl RangeBounds<f32>,
+    ) -> Option<TraceResult> {
+        let e1 = p2 - p1;
+        let e2 = p3 - p1;
+        let p = ray.direction.cross(e2);
+        let det = e1.dot(p);
+        if det.abs() < 1e-6 {
+            return None;
         }
+
+        let inv_det = 1.0 / det;
+        let t = (ray.origin - p1).dot(p) * inv_det;
+        if !range.contains(&t) {
+            return None;
+        }
+
+        let q = ray.origin - p1;
+        let u = q.dot(p) * inv_det;
+        if u < 0.0 || u > 1.0 {
+            return None;
+        }
+
+        let v = ray.direction.dot(q) * inv_det;
+        if v < 0.0 || u + v > 1.0 {
+            return None;
+        }
+
+        let point = ray.at(t);
+        let normal = (e1.cross(e2)).normalize();
+
+        let front_face = ray.direction.dot(normal) < 0.0;
+        let normal = if front_face { normal } else { -normal };
+
+        Some(TraceResult {
+            distance: t,
+            point,
+            normal,
+            geometry_index,
+            front_face,
+            uv: (u, v),
+        })
     }
 }
